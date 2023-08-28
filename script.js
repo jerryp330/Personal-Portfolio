390c57d03a880f8f4b0cbcd1b107f2ba5e533795
jerryp330 <110803469+jerryp330@users.noreply.github.com>
2023-08-27 19:11:28 -0600

    about me done

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+var image = document.getElementById('me2');
+var desiredWidth = 300; 
+var aspectRatio = image.naturalWidth / image.naturalHeight;
+var desiredHeight = desiredWidth / aspectRatio;
+
+image.style.width = desiredWidth + 'px';
+image.style.height = desiredHeight + 'px';
+image.style["border-radius"] = "20%";
